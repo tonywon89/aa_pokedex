e6c6d1f5ac2bebf482ebf3875a1568ac90acc781
Tony Won <tonywon89@gmail.com>
2016-04-21 12:12:41 -0700

    Implement react-routers for pokedex

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,8 +2,32 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 // PokemonStore = require('./stores/pokemon.js');
 // ApiUtils = require('./util/apiUtil');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+
 var PokemonsIndex = require('./components/pokemons/pokemonsIndex.jsx');
 
+
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(<PokemonsIndex/>, document.getElementById("root"));
+
+  var App = React.createClass({
+    render: function() {
+      return (
+        <div id="pokedex">
+          <div className="pokemon-index-pane">
+            <PokemonsIndex />
+          </div>
+        </div>
+      );
+    }
+  });
+
+  var routes = <Route path="/" component={App}> </Route>;
+
+  ReactDOM.render(
+    <Router>
+      {routes}
+     </Router>,
+    document.getElementById("root")
+  );
 });
